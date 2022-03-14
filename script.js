9bcc2c4be1b9db28274a09ec22c4a0263ffc83f6
Jeann Rezende <86562591+JeannRezende7@users.noreply.github.com>
2022-03-14 12:50:39 -0300

    v2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function adicionarFilme() {
   if (filmeImput.endsWith(".jpg")) {
     listarFilmesNaTela(filmeImput);
   } else {
-    console.error("Endereço de filme inválido");
+    alert("Endereço de filme inválido");
   }
   document.getElementById("filme").value = "";
 }
@@ -14,4 +14,20 @@ function listarFilmesNaTela(filmeImput) {
   var elementoFilme = "<img src= " + filmeImput + ">";
   var elementoListaFilmes = document.getElementById("listaFilmes");
   elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilme;
+}
+
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/1/14/Spide-Man_Poster.jpg/250px-Spide-Man_Poster.jpg",
+  "https://img.elo7.com.br/product/zoom/26776B4/big-poster-filme-homem-aranha-2-2004-lo01-tamanho-90x60-cm-marvel.jpg",
+  "https://img.elo7.com.br/product/zoom/2677953/big-poster-filme-homem-aranha-3-2007-lo02-tamanho-90x60-cm-homem-aranha.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_.jpg",
+  "https://i.pinimg.com/736x/4c/bd/eb/4cbdebda6ca9fec0b893eafc0910bd4c--man-movies-movies-free.jpg",
+  "https://hqcafe.com.br/wp-content/uploads/2020/07/critica-homem-aranha-de-volta-7023049.jpg",
+  "https://upload.wikimedia.org/wikipedia/pt/0/04/Spider-man-far-from-home-poster.jpg",
+  "https://ogimg.infoglobo.com.br/in/25277816-5b3-952/FT450A/WhatsApp-Image-2021-11-15-at-16.38.05-1.jpeg.jpg"
+];
+for (var i = 0; i < listaFilmes.length; i++) {
+  var elementoLista = "<img src= " + listaFilmes[i] + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoLista;
 }
